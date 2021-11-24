a04adf263cc980f69f5cacd6c0fb1ca5a556550b
ConnorMcfadden <2458736M@student.gla.ac.uk>
2021-11-24 21:42:57 +0000

    Update admin.py
    Corrects admin.py by correcting the fields for each

--- django-framework/main/admin.py
+++ django-framework/main/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from main.models import DataAnalyst, Journey
 
-class JourneyAdmin(admin.ModelAdmin):
+class DataAnalystAdmin(admin.ModelAdmin):
     list_display = ('name', 'username')
     list_filter = ('username',)
     exclude = ('name',)
-admin.site.register(Journey, JourneyAdmin)
+admin.site.register(DataAnalyst, DataAnalystAdmin)
 
-#class DataAnalystAdmin(admin.ModelAdmin):
-#    list_display = ('date', 'driver', 'destination', 'purpose', 'no_of_pass', 'start_time', 'end_time', 'speedo_start', 'speedo_finish')
-#    list_filter = ('driver', 'start_time', 'end_time')
-#    exclude = ('destination', 'purpose', 'no_of_pass', 'speedo_start', 'speedo_finish')
-#admin.site.register(DataAnalyst, DataAnalystAdmin)
+class JourneyAdmin(admin.ModelAdmin):
+    list_display = ('date', 'driver', 'destination', 'purpose', 'no_of_pass', 'start_time', 'end_time', 'speedo_start', 'speedo_finish')
+    list_filter = ('driver', 'start_time', 'end_time')
+    exclude = ('destination', 'purpose', 'no_of_pass', 'speedo_start', 'speedo_finish')
+admin.site.register(Journey, JourneyAdmin)
