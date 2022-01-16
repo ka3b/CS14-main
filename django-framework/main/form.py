2c26e862356b01beb4b7475fd27a19dc7d2ef346
q87718350 <q87718350@gmail.com>
2022-01-16 17:25:07 +0000

    change detail of form to fit finalization

--- django-framework/main/form.py
+++ django-framework/main/form.py
@@ -6,46 +6,60 @@ from django import forms
 
 
 class JourneyForm(forms.Form):
-    start_date = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date', 'class' : "formTextField"}))
+    start_date = forms.DateField(
+        label='select start date',
+        widget=forms.widgets.DateInput(attrs={'type': 'date', 'class' : "formTextField",
+                                              }))
 
-    end_date = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date', 'class' : "formTextField"}))
+    end_date = forms.DateField(
+        label='select end date',
+        widget=forms.widgets.DateInput(attrs={'type': 'date', 'class' : "formTextField"}))
 
     start_time = forms.TimeField(
-        label='Start Time', widget=forms.TimeInput(attrs={'placeholder':"Selected time", 'type':"time",
-                                                     'id':"appt", 'class' : "formTextField"}))
+        label='Select start time', widget=forms.TimeInput(attrs={'type':"time",
+                                                      'class' : "formTextField"}))
     end_time = forms.TimeField(
-        label='End Time', widget=forms.TimeInput(attrs={'placeholder':"Selected time", 'type':"time",
-                                                     'id':"appt", 'class' : "formTextField"}))
+        label='Select end time', widget=forms.TimeInput(attrs={'type':"time",
+                                                      'class' : "formTextField"}))
 
-    plate_number=forms.CharField(max_length=128, widget=forms.TextInput(attrs={
-        'type':'text1', 'class':"form-control formTextField", 'placeholder':'Type in Manually'
+    plate_number=forms.CharField(label='',max_length=128, widget=forms.TextInput(attrs={
+        'type':'text1', 'class':"form-control formTextField", 'placeholder':'License Plate'
     }))
 
     driver = forms.CharField(
+        label="",
         max_length=128,  widget=forms.TextInput(
-            attrs={'placeholder':"Name", 'class':"form-control formTextField", 'type':'text1', "id":'driver_name'}))
+            attrs={'placeholder':"Drive Name", 'class':"form-control formTextField", 'type':'text1',
+                   "id":'driver_name'}))
 
     CHOICES=(('1', 'Transport of goods'),('2','Picking up of goods'), ('3','Transport of people'),
             ('4','Fieldwork'))
-    purpose = forms.CharField(widget=forms.Select(choices=CHOICES,
-                              attrs={'class':"form-select formTextField", "aria-label":'Default select example'}))
+    purpose = forms.CharField(label='',widget=forms.Select(choices=CHOICES,
+                              attrs={'class':"form-select formTextField", "aria-label":'Default select example'
+                                     ,'placeholder':'Journey Purpose'}))
 
-    no_of_pass = forms.IntegerField(min_value=1,max_value=8,widget=forms.NumberInput(attrs={'class':"formTextField"}))
+    no_of_pass = forms.IntegerField(label='',min_value=1,max_value=8,widget=forms.NumberInput(attrs={
+        'class':"formTextField", 'placeholder':'Number of Passengers'}))
 
-    destinations=forms.CharField(max_length=256, widget=forms.TextInput(attrs={'class':"formTextField",
-                                                                              'label': 'Destination of The Trip'}))
-    mileage_start=forms.IntegerField(min_value=0, widget=forms.NumberInput(attrs={'class':"formTextField",
+    destinations=forms.CharField(label='',max_length=256, widget=forms.TextInput(attrs={'class':"formTextField",
+                                                                              'placeholder': 'Destination of The Trip'}))
+    mileage_start=forms.IntegerField(label='',min_value=0, widget=forms.NumberInput(attrs={'class':"formTextField",
                                                                               'label': 'Mileage Start Number',
-                                                                            'placeholder':'Enter start value'}))
-    mileage_finish = forms.IntegerField(min_value=0, widget=forms.NumberInput(attrs={'class': "formTextField",
+                                                                            'placeholder':'mileage start reading'}))
+    mileage_finish = forms.IntegerField(label='',min_value=0, widget=forms.NumberInput(attrs={'class': "formTextField",
                                                                       'label': 'Mileage End Number',
-                                                                        'placeholder':'Enter end value',
+                                                                        'placeholder':'mileage end reading',
                                                                         }))
-    is_round_trip=forms.BooleanField(required=False,
-                                     widget=forms.CheckboxInput(
-                                         attrs={'label':'Is round Trip',
-                                                'placeholder':'Is yes, please tick it'}))
-    
+
+
+    Trip_CHOICES = ((True, 'Round Trip'), (False, 'One-way Trip'))
+
+    is_round_trip=forms.BooleanField(required=True, label="",
+                                     widget=forms.RadioSelect(
+                                         choices=Trip_CHOICES))
+    #fuel_liter=forms.FloatField(widget=forms.NumberInput(attrs={'label':'Fuel Liter',
+                                                                #"placeholder": 'Please type in float number'}))
+
     approved_status=forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput())
 
     def __init__(self, *args, **kwargs):
@@ -55,9 +69,9 @@ class JourneyForm(forms.Form):
         self.helper.label_class = "col-md-2"
         self.helper.layout = Layout(
 
-            Fieldset('Date&Time', 'start_date', 'end_date', 'start_time', 'end_time'),
-            Fieldset('Vehicle Information','plate_number', 'mileage_start', 'mileage_finish'),
-            Fieldset('Journey Information', 'driver', 'purpose', 'no_of_pass', 'destinations',
+            Div(HTML('<h3>Date&Time</h3>'), 'start_date', 'end_date', 'start_time', 'end_time'),
+            Div(HTML('<h3>Vehicle Information</h3>'),'plate_number', 'mileage_start', 'mileage_finish'),
+            Div(HTML('<h3>Journey Information</h3>'), 'driver', 'purpose', 'no_of_pass', 'destinations',
                      Field('is_round_trip',css_id='tickbox')),
             Field('approved_status')
         )
